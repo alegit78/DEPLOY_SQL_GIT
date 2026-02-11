01c5c3cebcd24e6145598dc6bbb8bf3114b6ec0e
alegit78 <49518630+alegit78@users.noreply.github.com>
2026-02-11 16:19:59 +0100

    Update AdSApplicazioni.sql

--- TAB_TEST/AdSApplicazioni.sql
+++ TAB_TEST/AdSApplicazioni.sql
@@ -3,4 +3,6 @@
 --------------------------------------------------------
 
   CREATE TABLE "AdSApplicazioni2" ("AdS_applicazione_id" NUMBER, "dataDecorrenza" DATE, "ordine" NUMBER, "version" TIMESTAMP (6), "amministratoreDiSistema_amministratoreDiSistema_id_OID" NUMBER, "applicazione_applicazione_id_OID" NUMBER)
+/
+
 
