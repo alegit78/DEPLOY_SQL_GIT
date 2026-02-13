42e754bdcf3023f54002c1c6d50e1729b574ebcf
alegit78 <49518630+alegit78@users.noreply.github.com>
2026-02-13 17:06:20 +0100

    Update AdSApplicazioni.sql

--- TAB_TEST/AdSApplicazioni.sql
+++ TAB_TEST/AdSApplicazioni.sql
@@ -2,7 +2,8 @@
 --  DDL for Table AdSApplicazioni
 --------------------------------------------------------
 
-  CREATE TABLE AdSApplicazioni2 (AdS_applicazione_id NUMBER, dataDecorrenza DATE, ordine NUMBER, version TIMESTAMP (6), amministratoreDiSistema_amministratoreDiSistema_id_OID NUMBER, applicazione_applicazione_id_OID NUMBER)
+  CREATE TABLE AdSApplicazioni3 (AdS_applicazione_id NUMBER, dataDecorrenza DATE, ordine NUMBER, version TIMESTAMP (6), amministratoreDiSistema_amministratoreDiSistema_id_OID NUMBER, applicazione_applicazione_id_OID NUMBER)
+
 
 
 
