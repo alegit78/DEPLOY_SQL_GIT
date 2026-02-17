41c88e436c37c6ede35a44778df574e96d8b3040
alegit78 <49518630+alegit78@users.noreply.github.com>
2026-02-17 11:49:17 +0100

    Delete TABLES directory

--- TABLES/DatabaseAppCompArch.sql
+++
@@ -1,5 +0,0 @@
---------------------------------------------------------
---  DDL for Table DatabaseAppCompArch
---------------------------------------------------------
-
-  CREATE TABLE "DatabaseAppCompArch" ("database_appCompArch_id" NUMBER, "addendumTecnici" VARCHAR2(255), "database_database_id_OID" NUMBER, "version" TIMESTAMP (6), "applicazioneComponenteArchitettura_applicazione_compArch_id_OID" NUMBER)
