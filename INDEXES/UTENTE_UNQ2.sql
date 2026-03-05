38d22fd92a78b17767376fd8794be8c2dcbfe622
alegit78 <49518630+alegit78@users.noreply.github.com>
2026-03-05 12:23:02 +0100

    Add files via upload

---
+++ INDEXES/UTENTE_UNQ2.sql
@@ -0,0 +1,5 @@
+--------------------------------------------------------
+--  DDL for Index UTENTE_UNQ2
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "UTENTE_UNQ2" ON "UTENTE" ("DISTINGUISHNAME")
